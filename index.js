f7f3442700e94f4061d9d9d0e0dcc8605c48d9bf
bagusadlan <bagusadlann@gmail.com>
2024-01-31 12:07:53 +0700

    deploy: change url for cors origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(
   cors({
     credentials: true,
-    origin: ['https://to-do-app-bagus.vercel.app/', 'http://localhost:5173']
+    origin: 'https://to-do-app-bagus.vercel.app/'
   })
 )
 
