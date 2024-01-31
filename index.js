c8640d9bcaf6f99faf4e01af1a16d8024c801000
bagusadlan <bagusadlann@gmail.com>
2024-01-31 12:25:48 +0700

    deploy: url encoded

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ connectDB()
 
 const app = express()
 app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(
   cors({
     credentials: true,
-    origin: 'https://to-do-app-bagus.vercel.app/'
+    origin: 'https://to-do-app-bagus.vercel.app'
   })
 )
 
