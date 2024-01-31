eb60161212c4a85b12b4c7f31a1ac9f3b3baceaa
bagusadlan <bagusadlann@gmail.com>
2024-01-31 11:35:14 +0700

    feat(hash): add prototype to compare password for login

--- models/User.js
+++ models/User.js
@@ -28,4 +28,8 @@ userSchema.pre('save', async function (next) {
   }
 })
 
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password)
+}
+
 module.exports = mongoose.model('User', userSchema)
